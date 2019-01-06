2fd646721f9c131aab12c516d28064559713833e
Jieung Kim <je1004k@gmail.com>
2019-01-06 16:25:16 -0500

    CAL is done

--- source_code/ccal/lock.v
+++ source_code/ccal/lock.v
@@ -1,7 +1,7 @@
-Fixpoint <@$\replay_{\comm{ticket}}$@> (l: Log) (b: Loc) :=
+Fixpoint <@$\replay_{\codeinmath{ticket}}$@> (l: Log) (b: Loc) :=
   match l with
     | nil => (0, 0) (* ticket and now*)
-    | a :: l' => let (t, n):= <@$\replay_{\comm{ticket}}$@> l' b in
+    | a :: l' => let (t, n):= <@$\replay_{\codeinmath{ticket}}$@> l' b in
                  match a with
                    | <@$c.\incticket(b)$@> => (t + 1, n)
                    | <@$c.\incnow(b)$@> => (t, n + 1)
@@ -10,4 +10,4 @@ Fixpoint <@$\replay_{\comm{ticket}}$@> (l: Log) (b: Loc) :=
   end.
 Function <@$\spec_{\incticket}$@> <@$\oracle$@> (s: State) (b: Loc) :=
   let l':= <@$s.c.\incticket(b)$@> :: <@$\oracle$@>[s.c, s.l] in
-  let (t, n):= <@$\replay_{\comm{ticket}}$@> l' b in ret (s {l: l'}, t - 1).
+  let (t, n):= <@$\replay_{\codeinmath{ticket}}$@> l' b in ret (s {l: l'}, t - 1).
--- source_code/ccal/push.v
+++ source_code/ccal/push.v
@@ -1,8 +1,8 @@
-Fixpoint <@$\replay_{\comm{shared}}$@> (l: Log) (b: Loc) (c: Id) :=
+Fixpoint <@$\replay_{\codeinmath{shared}}$@> (l: Log) (b: Loc) (c: Id) :=
   match l with
     | nil => ret (<@$\vundef$@>, free) (* initial value *)
     | e :: l' =>  (* l' <@$\color{mygreen}\cons$@> a *)
-      do r <- <@$\replay_{\comm{shared}}$@> l' b; (* Haskell syntax sugar*)
+      do r <- <@$\replay_{\codeinmath{shared}}$@> l' b; (* Haskell syntax sugar*)
         match r, e with
           | (v, free), <@$c.\pull(b)$@> => ret (v, own c)
           | (_, own c), <@$c.\push(b, v)$@> => ret (v, free)
@@ -11,7 +11,7 @@ Fixpoint <@$\replay_{\comm{shared}}$@> (l: Log) (b: Loc) (c: Id) :=
   end.
 Function <@$\spec_{\pull}$@> <@$\oracle$@> (s: State) (b: Loc) :=
   let l':= <@$s.c.\pull(b)$@> :: <@$\oracle$@>[s.c, s.l] in (* query <@$\color{mygreen}\oracle$@> *)
-  do r <- <@$\replay_{\comm{shared}}$@> l' b s.c; ret s {l: l'} {m.b: fst r}.
+  do r <- <@$\replay_{\codeinmath{shared}}$@> l' b s.c; ret s {l: l'} {m.b: fst r}.
 Function <@$\spec_{\push}$@> <@$\oracle$@> (s: State) (b: Loc) :=
   let l':= <@$s.c.\push(b, s.m\ b)$@> :: s.l in (*do not query <@$\color{mygreen}\oracle$@>*)
-  do _ <- <@$\replay_{\comm{shared}}$@> l' b s.c; ret s {l: l'}.
+  do _ <- <@$\replay_{\codeinmath{shared}}$@> l' b s.c; ret s {l: l'}.
