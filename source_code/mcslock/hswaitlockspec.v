ae1a547cf0819508378bb18af3d769da407e61aa
Jieung Kim <je1004k@gmail.com>
2019-01-11 01:44:53 -0500

    first pass Section 2, Section 3, and Section 8 - need to work on Section 6, 7 (half of Section 7 - intro and high-leel idea) as well as Section 5 (is possible) during this weekend

--- source_code/mcslock/hswaitlockspec.v
+++ source_code/mcslock/hswaitlockspec.v
@@ -4,13 +4,13 @@ Definition mcs_wait_hlock_spec (bound index ofs :Z) (adt: RData) : option RData 
   | (true,  true, Some abid) =>
    match ZMap.get abid (multi_log adt), ZMap.get abid (lock adt) with
    | MultiDef l, LockFalse =>
-     let to := ZMap.get abid (multi_oracle adt) in
-     let l1 := (to cpu l) ++ l in
+     let <@$\mathcal{E}_{lock(abid)}$@> := ZMap.get abid (multi_oracle adt) in
+     let l1 := (<@$\mathcal{E}_{lock(abid)}$@> cpu l) ++ l in
      let l' := (TEVENT cpu (TTICKET (WAIT_LOCK (Z.to_nat bound)))) :: l1 in
      match H_CalLock l' with
      | Some _ =>
        Some adt {multi_log: ZMap.set abid (MultiDef l') (multi_log adt)}
-                {lock: ZMap.set abid LockOwn (lock adt)}
+                {lock: ZMap.set abid (LockOwn false) (lock adt)}
      | _ => None end
    | _, _ => None end
   | _ => None end.
--- source_code/mcslock/waitlockloop.v
+++ source_code/mcslock/waitlockloop.v
@@ -2,13 +2,13 @@ Fixpoint CalMCS_AcqWait (n: nat) (i : Z) l o : option MultiLog :=
   match n with 
     | O => None
     | S n' =>
-      let l' :=  (to i l) ++ l in
+      let l' :=  (<@$\mathcal{E}_{mcs(i)}$@> i l) ++ l in
       match CalMCSLock l' with
         | Some (MCSLOCK tail la _) =>
           match ZMap.get i la with
             | (false, _) => Some ((TEVENT i (TTICKET (GET_BUSY false))) :: l')
             | _ => 
-              CalMCS_AcqWait n' i ((TEVENT i (TTICKET (GET_BUSY true))) ::l') to
+              CalMCS_AcqWait n' i ((TEVENT i (TTICKET (GET_BUSY true))) ::l') <@$\mathcal{E}_{mcs(i)}$@>
           end
         | _ => None
       end
